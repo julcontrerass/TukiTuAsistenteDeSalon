9f56cb9d7e2df6504eba6ab0a95c6053b8d0e10f
julcontrerass <jjulian.contrerass@gmail.com>
2025-10-24 17:26:40 -0300

    Agrego menu para el usuario en el sidebar

--- TukiGestor/TukiGestor/Site.Master.designer.cs
+++ TukiGestor/TukiGestor/Site.Master.designer.cs
@@ -51,15 +51,6 @@ namespace TukiGestor
         protected global::System.Web.UI.HtmlControls.HtmlAnchor linkReporte;
 
         /// <summary>
-        /// Control linkConfiguracion.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor linkConfiguracion;
-
-        /// <summary>
         /// Control MainContent.
         /// </summary>
         /// <remarks>
