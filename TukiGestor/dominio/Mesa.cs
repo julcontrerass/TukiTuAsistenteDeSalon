5b10aafcc5a5a017ed1d79fa9dd7fd6e5c08f30d
nemi1414 <juanignacionemi@gmail.com>
2025-10-31 10:27:34 -0300

    Hice pequeños cambios en los atributos de las clases

--- TukiGestor/dominio/Mesa.cs
+++ TukiGestor/dominio/Mesa.cs
@@ -9,8 +9,7 @@ namespace dominio
     public class Mesa
     {
         public int MesaId { get; set; }
-       // public string NombreMesa { get; set; }
-        //public int NumeroMesa { get; set; }
+        public int NumeroMesa { get; set; }
         public int Capacidad { get; set; }
         public string Ubicacion { get; set; }
         public string Estado { get; set; }
--- TukiGestor/dominio/Producto.cs
+++ TukiGestor/dominio/Producto.cs
@@ -10,7 +10,7 @@ namespace dominio
     {
         public int ProductoId { get; set; }
         public string Nombre { get; set; }
-       // public string Descripcion { get; set; }
+        public string Descripcion { get; set; }
         public decimal Precio { get; set; }
         public bool Disponible { get; set; }
         public int CategoriaId { get; set; }
