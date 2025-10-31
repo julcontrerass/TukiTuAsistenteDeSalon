949067ec37ac4dfe6f177165b7253018ad412620
nemi1414 <juanignacionemi@gmail.com>
2025-10-31 15:58:50 -0300

    Creé la capa y clase de AccesoDatos

---
+++ TukiGestor/AccesoDatos/AccesoDatos.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace accesoDatos
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public SqlCommand Comando
+        {
+            get { return comando; }
+        }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=TUKI_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+        public void SetearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+            comando.Parameters.Clear();
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+        public object ejecutarScalar()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                return comando.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error de SQL al ejecutar scalar: " + ex.Message, ex);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error de SQL al ejecutar accion: " + ex.Message, ex);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+    }
+}
